bcc9a71ae8e32a2a992542837028216dbc2b3bec
Brandon Hartshorn <brandonhartshorn@gmail.com>
2015-11-03 19:43:07 -0700

    Stubbed out Set and Excercise

--- app/src/main/java/edu/byui/cs246/prettyripped/models/Exercise.java
+++ app/src/main/java/edu/byui/cs246/prettyripped/models/Exercise.java
@@ -3,5 +3,24 @@ package edu.byui.cs246.prettyripped.models;
 /**
  * Created by bradb on 11/3/2015.
  */
-public class Exercise {
+public class Exercise implements IExercise{
+    @Override
+    public String getGroup() {
+        return null;
+    }
+
+    @Override
+    public void setGroup(String group) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
 }
--- app/src/main/java/edu/byui/cs246/prettyripped/models/Set.java
+++ app/src/main/java/edu/byui/cs246/prettyripped/models/Set.java
@@ -3,5 +3,24 @@ package edu.byui.cs246.prettyripped.models;
 /**
  * Created by bradb on 11/3/2015.
  */
-public class Set {
+public class Set implements ISet {
+    @Override
+    public int getReps() {
+        return 0;
+    }
+
+    @Override
+    public void setReps(int reps) {
+
+    }
+
+    @Override
+    public float getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(float weight) {
+
+    }
 }
