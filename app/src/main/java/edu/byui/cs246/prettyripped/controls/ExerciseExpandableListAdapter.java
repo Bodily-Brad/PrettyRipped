0899f80e6d889083faa63e7fdff23019799112e6
Bodily-Brad <bod12002@byui.edu>
2015-11-13 15:10:32 -0700

    Created ExerciseExpandableListAdapter Added support for the expandable list

---
+++ app/src/main/java/edu/byui/cs246/prettyripped/controls/ExerciseExpandableListAdapter.java
@@ -0,0 +1,162 @@
+package edu.byui.cs246.prettyripped.controls;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.byui.cs246.prettyripped.R;
+import edu.byui.cs246.prettyripped.models.IExercise;
+import edu.byui.cs246.prettyripped.models.ISet;
+
+/**
+ * Created by bradb on 11/13/2015.
+ */
+public class ExerciseExpandableListAdapter extends BaseExpandableListAdapter {
+
+    // LOCAL VARIABLES
+    private Context context;
+    private List<IExercise> exercises = new ArrayList<>();
+
+    /**
+     * Creates a new instance of ExerciseExpandableListAdapter
+     * @param context
+     * @param exercises
+     */
+    public ExerciseExpandableListAdapter(Context context, List<IExercise> exercises) {
+        this.context = context;
+        this.exercises = exercises;
+    }
+
+    /**
+     * Gets the number of groups (i.e. the number of exercises within our exercise list)
+     * @return the number of groups
+     */
+    @Override
+    public int getGroupCount() {
+        return exercises.size();
+    }
+
+    /**
+     * Gets the number of children within a particular group
+     * @param groupPosition the position of the group in question
+     * @return the number of children within the specified group
+     */
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return exercises.get(groupPosition).getSets().size();
+    }
+
+    /**
+     * Gets the Exercise at the specified position
+     * @param groupPosition position of the Exercise within the exercises list
+     * @return the Exercise at the specified location within the exercises list
+     */
+    @Override
+    public Object getGroup(int groupPosition) {
+        return exercises.get(groupPosition);
+    }
+
+    /**
+     * Gets a Set from the child list
+     * @param groupPosition position of the Exercise within the exercise list to get the set
+     *                      from
+     * @param childPosition position of the Set within the Exercise's set list to get the set
+     *                      from
+     * @return the Set at the specified position within the specified Exercise
+     */
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        List<ISet> sets = exercises.get(groupPosition).getSets();
+        return sets.get(childPosition);
+    }
+
+    /**
+     * Returns the id for a particular groupPosition
+     * @param groupPosition groupPosition in question
+     * @return the id of the groupPosition
+     */
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    /**
+     * Returns the id for a particular childPosition
+     * @param groupPosition groupPosition of the childPosition in question
+     * @param childPosition childPosition in question
+     * @return the id of the childPosition
+     */
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    /**
+     * Gets a view for a group element (Exercise)
+     * @param groupPosition group position within the exercises list
+     * @param isExpanded
+     * @param convertView destination view
+     * @param parent parent ViewGroup
+     * @return a view representing the group element
+     */
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        IExercise exercise = exercises.get(groupPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.exercise_view, null);
+        }
+
+        CheckedTextView label = (CheckedTextView) convertView.findViewById(R.id.labelExerciseName);
+        label.setText(exercise.getName());
+
+        return convertView;
+    }
+
+    /**
+     * Gets a view for a child element (Set)
+     * @param groupPosition group position within the exercises list
+     * @param childPosition child position within the specific exercise
+     * @param isLastChild boolean indicator as to whether this is the last child in the list
+     * @param convertView destination view
+     * @param parent parent ViewGroup
+     * @return a view representing the child element
+     */
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        // Get the child for the specified group/child position
+        ISet set = (ISet) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.set_view, null);
+        }
+
+        EditText editWeight = (EditText) convertView.findViewById(R.id.editWeight);
+        EditText editReps = (EditText) convertView.findViewById(R.id.editReps);
+
+        editWeight.setText( Float.toString( set.getWeight() ) );
+        editReps.setText( Float.toString( set.getReps() ) );
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
--- app/src/main/java/edu/byui/cs246/prettyripped/views/SessionActivity.java
+++ app/src/main/java/edu/byui/cs246/prettyripped/views/SessionActivity.java
@@ -1,15 +1,36 @@
 package edu.byui.cs246.prettyripped.views;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.byui.cs246.prettyripped.R;
+import edu.byui.cs246.prettyripped.controls.ExerciseExpandableListAdapter;
+import edu.byui.cs246.prettyripped.models.Exercise;
+import edu.byui.cs246.prettyripped.models.IExercise;
+import edu.byui.cs246.prettyripped.models.ISet;
+import edu.byui.cs246.prettyripped.models.Set;
 
 public class SessionActivity extends AppCompatActivity {
+    // LOCAL VARIABLES
+    private List<IExercise> exercises;
+
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +52,66 @@ public class SessionActivity extends AppCompatActivity {
         // To-do: Get rid of this, this is just a test to see if it will
         // set the title in the "blue bar"
         this.setTitle("Date");
+
+        // Make up some data
+        createDefaultData();
+
+        // Set up expandable list
+        listView = (ExpandableListView) findViewById(R.id.exerciseList);
+
+        // Create adapter
+        listAdapter = new ExerciseExpandableListAdapter(SessionActivity.this, exercises);
+
+        // Attach adapter to list
+        listView.setAdapter(listAdapter);
+    }
+
+    private void createDefaultData() {
+        // Just make some dummy data
+        exercises = new ArrayList<IExercise>();
+
+        IExercise ex;
+        ISet set;
+
+        // Exercise 1
+        ex = new Exercise();
+        ex.setName("Curl");
+
+        // Set of 10lb x 5
+        set = new Set();
+        set.setReps(5);
+        set.setWeight(10);
+        ex.addSet(set);
+
+        // Set of 12 lbs x 6
+        set = new Set();
+        set.setReps(6);
+        set.setWeight(12);
+        ex.addSet(set);
+
+        exercises.add(ex);
+
+        // Exercise 2
+        ex = new Exercise();
+        ex.setName("Bench Press");
+
+        // Set of 60lb x 5
+        set = new Set();
+        set.setReps(5);
+        set.setWeight(60);
+        ex.addSet(set);
+
+        // Set of 72 lbs x 6
+        set = new Set();
+        set.setReps(6);
+        set.setWeight(72);
+        ex.addSet(set);
+
+        exercises.add(ex);
+
+
+
+
     }
 
 }
