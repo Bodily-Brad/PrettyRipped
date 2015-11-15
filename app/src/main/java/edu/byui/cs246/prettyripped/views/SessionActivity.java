3aff703ef71bbfe05d10903170f42a4956ac2602
Brandon Hartshorn <brandonhartshorn@gmail.com>
2015-11-14 20:22:31 -0700

    Changed some padding on the layout to make it look a bit nicer. Cleaned up some imports.

--- app/src/main/java/edu/byui/cs246/prettyripped/views/SessionActivity.java
+++ app/src/main/java/edu/byui/cs246/prettyripped/views/SessionActivity.java
@@ -1,17 +1,11 @@
 package edu.byui.cs246.prettyripped.views;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.CheckedTextView;
-import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
