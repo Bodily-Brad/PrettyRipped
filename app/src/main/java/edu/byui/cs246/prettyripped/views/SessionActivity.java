41da6233c84f9355f62a9107e27847286330453e
Bodily-Brad <bod12002@byui.edu>
2015-12-07 15:56:24 -0700

    DatePicker works, but the UI doesn't get refreshed yet (you can verify the date has changed by leaving the session, then returning to it - the date will be wrong in the SessionS list until the app is restarted, however)

--- app/src/main/java/edu/byui/cs246/prettyripped/views/SessionActivity.java
+++ app/src/main/java/edu/byui/cs246/prettyripped/views/SessionActivity.java
@@ -1,6 +1,8 @@
 package edu.byui.cs246.prettyripped.views;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,10 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import edu.byui.cs246.prettyripped.PrettyRippedData;
@@ -60,7 +66,7 @@ public class SessionActivity extends AppCompatActivity {
         PrettyRippedData data = PrettyRippedData.getInstance();
 
         // Get passed session ID
-        long sessionID = getIntent().getLongExtra(this.SESSION_ID_KEY, 0);
+        final long sessionID = getIntent().getLongExtra(this.SESSION_ID_KEY, 0);
 
         // Get session from data handler
         session = data.getSessionById(sessionID);
@@ -112,6 +118,41 @@ public class SessionActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Context context = this;
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(session.getTime());
+        final Session passedSession = session;
+
+        // Set up long press on title
+        toolbar.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                DatePickerDialog dialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        // Get data handler
+                        PrettyRippedData data = PrettyRippedData.getInstance();
+
+                        // Make a calendar and all the junk that you have to do to get a date
+                        Calendar cal = new GregorianCalendar();
+                        cal.set(year, monthOfYear, dayOfMonth);
+
+                        // Assign the time from the picker to the session
+                        passedSession.time = cal.getTime();
+
+                        // Update the session with the handler
+                        data.updateSession(passedSession);
+
+                        // TODO: Update view with new date
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+                dialog.show();
+                return false;
+            }
+        });
     }
 
     public void refreshList() {
